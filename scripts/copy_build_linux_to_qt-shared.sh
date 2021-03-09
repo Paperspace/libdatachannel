f0321b0a18bd5e54cde4db78171db0d4ad672581
Tom Sanfilippo <sanfilip@gmail.com>
2021-03-08 22:12:00 -0500

    script for copy of linux build to qt-shared

---
+++ scripts/copy_build_linux_to_qt-shared.sh
@@ -0,0 +1,10 @@
+#/usr/bin/env bash
+PS_QT=/mnt/c/ps
+mkdir -p ${PS_QT}/qt-shared/libs/libdatachannel/build/linux/release
+cp ../../libdatachannel_build_linux_relsym_with_static_openssl/libdatachannel.so.0.11.4 ${PS_QT}/qt-shared/libs/libdatachannel/build/linux/release/
+rm -f ${PS_QT}/qt-shared/libs/libdatachannel/build/linux/release/libdatachannel.so
+ln -rs ${PS_QT}/qt-shared/libs/libdatachannel/build/linux/release/libdatachannel.so.0.11.4 ${PS_QT}/qt-shared/libs/libdatachannel/build/linux/release/libdatachannel.so
+mkdir -p ${PS_QT}/qt-shared/libs/libdatachannel/build/linux/debug
+cp ../../libdatachannel_build_linux_debug_with_static_openssl/libdatachannel.so.0.11.4 ${PS_QT}/qt-shared/libs/libdatachannel/build/linux/debug/
+rm -f ${PS_QT}/qt-shared/libs/libdatachannel/build/linux/debug/libdatachannel.so
+ln -rs ${PS_QT}/qt-shared/libs/libdatachannel/build/linux/debug/libdatachannel.so.0.11.4 ${PS_QT}/qt-shared/libs/libdatachannel/build/linux/debug/libdatachannel.so
