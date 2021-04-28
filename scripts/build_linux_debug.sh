22f2340bfc5a9ff148be2289e614991f690ae8b5
benjamin <benjamin@paperspace.com>
2021-04-28 11:11:06 -0400

    static build changes for linux

--- scripts/build_linux_debug.sh
+++ scripts/build_linux_debug.sh
@@ -4,8 +4,9 @@ set -e
 # in libdatachannel/scripts directory
 
 cd ..
-OPENSSL_ROOT_DIR=/home/paperspace/projects/PS_OPENSSL/release/linux111u18/ cmake -DUSE_GNUTLS=0 -DUSE_NICE=0 -DOPENSSL_USE_STATIC_LIBS=TRUE -DCMAKE_BUILD_TYPE=Debug
+
+OPENSSL_ROOT_DIR=/home/paperspace/projects/PS_OPENSSL/release/linux111u18d/ cmake -B ../libdatachannel_build_linux_debug_with_static_openssl -DUSE_GNUTLS=0 -DUSE_NICE=0 -DOPENSSL_USE_STATIC_LIBS=TRUE -DCMAKE_BUILD_TYPE=Debug
+
+cd ../libdatachannel_build_linux_debug_with_static_openssl
 make
-
-
 
